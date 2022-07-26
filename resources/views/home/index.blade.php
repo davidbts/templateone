196bad7cad754e83dee46c4f022a1267588be67d
David Trejo <davidbts.escolar@gmail.com>
2022-07-26 18:10:36 -0500

    Archivos blade

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,32 +2,6 @@
 
 @section('content')
 
-
-    @if (session('menor'))
-        <h1> {{ session('menor') }} </h1>
-    @endif
-
-    @if (Auth::user() && session('prender'))
-        {{ session()->forget('prender') }}
-    @endif
-
-    <script>
-        function abrir() {
-            const modaluser = document.querySelector(".modal-user");
-            modaluser.style.display = 'block';
-            const modaluserreg = document.querySelector(".modal-login");
-            modaluserreg.style.display = 'block';
-        }
-        {{ session('prender') }}
-    </script>
-
-
-    @include('includes.carrusel')
-
-    @include('includes.categorias')
-
-    @include('includes.prenda')
-
-    @include('includes.mascategorias')
+<h1>Principal</h1>
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,42 +16,14 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
-        crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 
 </head>
 
 <body>
 
-    <?php
-    $categories = DB::table('categories')
-        ->orderBy('id', 'asc')
-        ->get();
-
-    $subcategories = DB::table('subcategories')
-        ->orderBy('id', 'asc')
-        ->get();
-
-    $elements = DB::table('elements')
-        ->orderBy('id', 'asc')
-        ->get();
-    ?>
-
-    <div class="modal-user">
-        <div class="contenedor-modal">
-            @include('autenticacion.register')
-            @include('autenticacion.login')
-            @include('autenticacion.recuperar')
-            @include('includes.invitaamigos')
-        </div>
-    </div>
-
-    @include('includes.header', [
-    'categories' => $categories,
-    'subcategories' => $subcategories,
-    'elements' => $elements,
-    ])
-
+    @include('includes.header')
+    
     <div class="content">
         @yield('content')
     </div>
